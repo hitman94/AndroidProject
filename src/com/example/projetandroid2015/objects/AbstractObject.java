7cf8a9bc7ab2f8f50e2702bc9c8a0a254bfb2c64
Thomas Guerin <adminmizix@gmail.com>
2015-02-24 21:37:13 +0100

    Objet de base

---
+++ src/com/example/projetandroid2015/objects/AbstractObject.java
@@ -0,0 +1,20 @@
+package com.example.projetandroid2015.objects;
+
+import java.util.Map;
+
+public class AbstractObject {
+	
+	private Long id;
+	
+	private String name;
+	
+	private boolean sealed;
+	
+	private Map<String, Object> objects;
+	
+	private Map<String, SpecialObject<Object>> specialObjects;
+	
+	
+	
+
+}
